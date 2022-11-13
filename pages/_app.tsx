a8d308e06ef0fb951e76e20b4184a16290e86b2b
ERIC ALEJANDRO AGUILAR MARCIAL <a338817@uach.mx>
2022-11-13 06:30:16 -0600

    mini changes 4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import { MetaMaskInpageProvider } from '@metamask/providers';
 declare var window: any
 export default function App({ Component, pageProps }: AppProps) {
   const [walletAddress, setWalletAddress] = useState("");
 
   const getCurrentWalletConnected = async () => {
     if(typeof window.ethereum !== "undefined"){
+      const ethereum = window.ethereum as MetaMaskInpageProvider;
       ethereum.request({ method: 'eth_requestAccounts' })
       .then((wallets:any) => setWalletAddress(wallets[0]))
     }
